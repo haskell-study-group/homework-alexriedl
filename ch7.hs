47b7dea62172977445e03823e2f267bc34207361
Alex Riedl <alexanderbriedl@gmail.com>
2016-11-17 23:32:44 -0600

    1-4 on ch7

--- ch7.hs
+++ ch7.hs
@@ -1,4 +1,36 @@
 --One
 
 --one :: (a -> b) (a -> Bool) -> [a] -> [a]
-one m f (x) = map m (filter f x)
+one m f (x) = Prelude.map m (filter f x)
+
+--Two
+all :: (a -> Bool) -> [a] -> Bool
+all f [x] = f x
+all f (s:xs) = (Main.all f xs) && f s
+
+any :: (a -> Bool) -> [a] -> Bool
+any f [x] = f x
+any f (s:xs) = (Main.any f xs) || f s
+
+takeWhile :: (a -> Bool) -> [a] -> [a]
+takeWhile f [] = []
+takeWhile f (x:xs) = do
+    if f x
+       then (x:Main.takeWhile f xs)
+       else []
+
+dropWhile :: (a -> Bool) -> [a] -> [a]
+dropWhile f [] = []
+dropWhile f (x:xs) = do
+    if f x
+       then (Main.dropWhile f xs)
+       else (x:xs)
+
+--Three
+--map :: (a -> b) -> [a] -> [b]
+--map f (x) = (foldr f []) x
+
+--Four
+dec2int :: [Int] -> Int
+dec2int [x] = x
+dec2int (x:xs) = foldl (\a1 a2 -> a1 * 10 + a2) x xs
