546a593539cafd6f8161e7fd9076a25508185d0a
Alex Riedl <alexanderbriedl@gmail.com>
2017-01-06 08:11:43 -0600

    ch8

--- ch8.hs
+++ ch8.hs
@@ -49,3 +49,58 @@ size (Add a b) = size a + size b
 
 --size2 :: Expr -> Int
 --size2 = fold (
+
+--Seven
+data Optional a = None | Some a
+data List a = Empty | Cons a (List a)
+
+instance Eq a => Eq (Optional a) where
+    None == None = True
+    (Some a) == (Some b) = a == b
+    _ == _ = False
+
+{-|
+instance Eq a => Eq List a where
+    Empty == Empty = True
+    (x:xs) == (y:ys) = x == y && xs == ys
+    _ == _ = False
+-}
+
+
+--Eight
+data Prop = Const Bool
+          | Var Char
+          | Not Prop
+          | And Prop Prop
+          | Imply Prop Prop
+
+p1 :: Prop
+p1 = And (Var 'A') (Not (Var 'A'))
+
+type Assoc k v = [(k,v)]
+type Subst = Assoc Char Bool
+
+find :: Eq k => k -> Assoc k v -> v
+find k t = head [v | (k',v) <- t, k == k']
+
+evalProp :: Subst -> Prop -> Bool
+evalProp _ (Const b)    = b
+evalProp s (Var x)      = find x s
+evalProp s (Not p)      = not (evalProp s p)
+evalProp s (And p q)    = evalProp s p && evalProp s q
+evalProp s (Imply p q ) = evalProp s p <= evalProp s q
+
+vars :: Prop -> [Char]
+vars (Const _)   = []
+vars (Var x)     = [x]
+vars (Not p)     = vars p
+vars (And p q)   = vars p ++ vars q
+vars (Imply p q) = vars p ++ vars q
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False:) bss ++ map (True:) bss
+          where bss = bools (n-1)
+
+--Nine
+
